111bc05f2eb3d279d1fd0a43a34cff40e4ea2236
Marcel van Doornen <vandoornenmarcel@gmail.com>
2021-06-11 07:45:08 +0200

    Some code improvements

--- src/Traits/isAdmin.php
+++ src/Traits/isAdmin.php
@@ -4,20 +4,21 @@ namespace Mvd81\LaravelIsAdmin\Traits;
 trait isAdmin {
 
     public function isAdmin() {
-        return $this->is_admin || (config()->has('is_admin.use_super_admin') && config('is_admin.use_super_admin') && $this->id == 1);
+        return $this->is_admin
+            || (config()->has('is_admin.use_super_admin')
+                && config('is_admin.use_super_admin')
+                && $this->id == 1);
     }
 
     public function makeAdmin() {
         if ($this->id > 0) {
-            $this->is_admin = 1;
-            $this->save();
+            $this->update(['is_admin' => 1]);
         }
     }
 
     public function undoAdmin() {
         if ($this->id > 0) {
-            $this->is_admin = 0;
-            $this->save();
+            $this->update(['is_admin' => 0]);
         }
     }
 
